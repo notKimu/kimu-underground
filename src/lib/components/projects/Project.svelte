acfedb10dc58988b08e14633c5eebb41c3703fe5
kimu <notkimu@proton.me>
2024-11-06 16:44:48 +0100

    Added projects page

---
+++ src/lib/components/projects/Project.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  export let name: string;
+  export let description: string;
+  export let img: string;
+</script>
+
+<div class="project-container">
+  <img src={img} alt="" />
+
+  <div class="project-info">
+    <h3>{name}</h3>
+    <p>{description}</p>
+  </div>
+</div>
+
+<style>
+  .project-container {
+    position: relative;
+    height: 5rem;
+    display: flex;
+    gap: var(--padding-s);
+    background-color: var(--color-dark);
+    overflow: hidden;
+    padding: var(--padding-s);
+  }
+
+  .project-container:hover::before {
+    content: "";
+    position: absolute;
+    top: -50%;
+    left: -20%;
+    height: 10rem;
+    width: 5rem;
+
+    background-color: var(--color-fg);
+    opacity: 0.1;
+    transform: skewX(-25deg);
+    animation: shine 2s;
+  }
+
+  @keyframes shine {
+    0% {
+      left: -20%;
+      filter: blur(10px);
+    }
+    50% {
+      filter: blur(0px);
+    }
+    100% {
+      left: 110%;
+      filter: blur(10px);
+    }
+  }
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,3 +1,35 @@
+<script lang="ts">
+  import { _ } from "svelte-i18n";
+  import Separator from "$lib/components/Separator.svelte";
+  import Project from "$lib/components/projects/Project.svelte";
+  // IMAGES
+  import Rubiu5 from "$lib/images/projects/rubiu5.webp";
+  import Archan from "$lib/images/projects/archan.webp";
+  import StarMC from "$lib/images/projects/starmc.png";
+  import DnDAbenyu from "$lib/images/projects/dnd_abenyu.webp";
+  import Ryoko from "$lib/images/projects/ryoko.webp";
+</script>
+
 <svelte:head>
-	<title>My projects</title>
+  <title>My projects</title>
 </svelte:head>
+
+<h1 class="title">{$_("page.projects.title")}</h1>
+
+<Separator />
+
+<div class="project-list">
+  <Project name={"Rubiu5"} description={"The discord bot for Rubius Army"} img={Rubiu5} />
+  <Project name={"Archan"} description={"A complete discord bot featuring a control panel"} img={Archan} />
+  <Project name={"StarMC"} description={"Minecraft launcher built on Tauri"} img={StarMC} />
+  <Project name={"DnD Abenyu"} description={"A DnD helper server built in Rust"} img={DnDAbenyu} />
+  <Project name={"Ryoko"} description={"Social Minecraft plugin similar to Essentials X"} img={Ryoko} />
+</div>
+
+<style>
+  .project-list {
+    display: flex;
+    flex-direction: column;
+    gap: var(--padding-m);
+  }
+</style>
