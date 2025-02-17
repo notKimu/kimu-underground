bb651febe1e15713b3b62d0acc52499d36993f8c
notkimu <notkimu@proton.me>
2025-02-17 22:38:47 +0100

    Updated to new module syntax in story blueprint

--- src/lib/blueprints/story.svelte
+++ src/lib/blueprints/story.svelte
@@ -1,4 +1,4 @@
-<script lang="ts" context="module">
+<script lang="ts" module>
     export { default as h2 } from "$lib/blueprints/story/h2.svelte";
     export { default as p } from "$lib/blueprints/story/p.svelte";
 
