12fb5ca99f7d2811e0e92d9f21168ba234f75c46
notkimu <notkimu@proton.me>
2025-02-18 04:33:00 +0100

    Updated the banner and the about me tools and apps

--- src/lib/components/about/Apps.svelte
+++ src/lib/components/about/Apps.svelte
@@ -7,72 +7,83 @@
     import LibreWolfImg from "$lib/assets/images/applications/librewolf.webp?enhanced";
 </script>
 
-<div>
+<div class="apps-container">
     <div class="app-entry">
         <div class="app-image">
-            <enhanced:img src={VSCodiumImg} alt="" loading="lazy">
-            </enhanced:img>
+            <enhanced:img src={VSCodiumImg} alt="" loading="lazy" />
         </div>
         <p>VSCodium</p>
     </div>
 
     <div class="app-entry">
         <div class="app-image">
-            <enhanced:img src={NeovimImg} alt="" loading="lazy"> </enhanced:img>
+            <enhanced:img src={NeovimImg} alt="" loading="lazy" />
         </div>
         <p>Neovim</p>
     </div>
 
     <div class="app-entry">
         <div class="app-image">
-            <enhanced:img src={WinampImg} alt="" loading="lazy"> </enhanced:img>
+            <enhanced:img src={WinampImg} alt="" loading="lazy" />
         </div>
         <p>Winamp</p>
     </div>
 
     <div class="app-entry">
         <div class="app-image">
-            <enhanced:img src={ObsidianImg} alt="" loading="lazy">
-            </enhanced:img>
+            <enhanced:img src={ObsidianImg} alt="" loading="lazy" />
         </div>
         <p>Obsidian</p>
     </div>
 
     <div class="app-entry">
         <div class="app-image">
-            <enhanced:img src={KritaImg} alt="" loading="lazy"> </enhanced:img>
+            <enhanced:img src={KritaImg} alt="" loading="lazy" />
         </div>
         <p>Krita</p>
     </div>
 
     <div class="app-entry">
         <div class="app-image">
-            <enhanced:img src={LibreWolfImg} alt="" loading="lazy">
-            </enhanced:img>
+            <enhanced:img src={LibreWolfImg} alt="" loading="lazy" />
         </div>
         <p>LibreWolf</p>
     </div>
 </div>
 
 <style>
+    .apps-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: var(--padding-m);
+    }
+
     .app-entry {
         position: relative;
         display: flex;
+        flex-direction: column;
+        justify-content: center;
         align-items: center;
-        gap: var(--padding-m);
+        gap: var(--padding-s);
         padding: var(--padding-s) var(--padding-m);
     }
-    .app-entry:not(:last-child) {
-        border-bottom: var(--border-width) solid var(--color-border);
-    }
-
+    
     .app-image {
+        position: relative;
         height: 100%;
-        width: 3rem;
+        width: 3.5rem;
         display: grid;
         place-items: center;
         height: fit-content;
         padding: var(--padding-s);
+    }
+    .app-image::after {
+        content: url("data:image/ico;base64, AAABAAIAICACAAEAAQAwAQAAJgAAACAgEAABAAQA6AIAAFYBAAAoAAAAIAAAAEAAAAABAAEAAAAAAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP///wAAAAAAAAAAAAAAAAAAAAAAH///+B////gfv//4Hz//+B4f//geH//4Hg//+B4P//geB//4HwP/+B+B//gfgP54H8B8eB/gPHgf8Ah4H/AAeB/8AHgf/wB4H//AeB/+AHgf/gB4H///+B////gf///4AAAAAAAAAAAAAAAAAAAAAP///////////////8AAAAPAAAADwAAAA8AAAAPAAAADwAAAA8AAAAPAAAADwAAAA8AAAAPAAAADwAAAA8AAAAPAAAADwAAAA8AAAAPAAAADwAAAA8AAAAPAAAADwAAAA8AAAAPAAAADwAAAA8AAAAPAAAAD////////////////KAAAACAAAABAAAAAAQAEAAAAAAAAAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAIAAAACAgACAAAAAgACAAICAAADAwMAAgICAAAAA/wAA/wAAAP//AP8AAAD/AP8A//8AAP///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/////////////////wAAAP////////////////8AAAD///8P////////////AAAA///wD////////////wAAAP//8AD///////////8AAAD///AAD///////////AAAA///wAA///////////wAAAP//8AAP//////////8AAAD///AAAP//////////AAAA///wAAAP/////////wAAAP///wAAAP////////8AAAD///8AAAAP////AP//AAAA////8AAAAP//8AD//wAAAP////8AAAAP//AA//8AAAD/////8AAAAA8AAP//AAAA//////AAAAAAAAD//wAAAP//////8AAAAAAA//8AAAD////////wAAAAAP//AAAA//////////AAAAD//wAAAP///////wAAAAAA//8AAAD///////8AAAAAAP//AAAA/////////////////wAAAP////////////////8AAAD/////////////////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP///////////////8AAAAPAAAADwAAAA8AAAAPAAAADwAAAA8AAAAPAAAADwAAAA8AAAAPAAAADwAAAA8AAAAPAAAADwAAAA8AAAAPAAAADwAAAA8AAAAPAAAADwAAAA8AAAAPAAAADwAAAA8AAAAPAAAAD////////////////");
+        position: absolute;
+        left: -25%;
+        bottom: -20%;
+        transform: scale(55%);
     }
     .app-image img {
         height: auto;
--- src/lib/components/about/Tools.svelte
+++ src/lib/components/about/Tools.svelte
@@ -1,91 +1,181 @@
 <script lang="ts">
-    import GitImg from "$lib/assets/images/tools/git.webp?enhanced";
-    import BashImg from "$lib/assets/images/tools/bash.webp?enhanced";
+    // Images
     import RustImg from "$lib/assets/images/tools/rust.webp?enhanced";
+    import CImg from "$lib/assets/images/tools/C.webp?enhanced";
     import TypeScriptImg from "$lib/assets/images/tools/typescript.webp?enhanced";
     import JavaScriptImg from "$lib/assets/images/tools/javascript.webp?enhanced";
-    import CImg from "$lib/assets/images/tools/C.webp?enhanced";
     import PythonImg from "$lib/assets/images/tools/python.webp?enhanced";
+    import KotlinImg from "$lib/assets/images/tools/kotlin.png?enhanced";
     import JavaImg from "$lib/assets/images/tools/java.webp?enhanced";
     import GoImg from "$lib/assets/images/tools/golang.webp?enhanced";
+    import BashImg from "$lib/assets/images/tools/bash.webp?enhanced";
+    import SvelteImg from "$lib/assets/images/tools/svelte.png?enhanced";
+    import VueImg from "$lib/assets/images/tools/vue.png?enhanced";
+    import AngularImg from "$lib/assets/images/tools/angular.png?enhanced";
+    import NextJsImg from "$lib/assets/images/tools/nextjs.png?enhanced";
+    import ReactImg from "$lib/assets/images/tools/react.png?enhanced";
+    import BootstrapImg from "$lib/assets/images/tools/bootstrap.png?enhanced";
+    import TailwindImg from "$lib/assets/images/tools/tailwind.png?enhanced";
+    import RocketRsImg from "$lib/assets/images/tools/rocket.rs.png?enhanced";
     import MySqlImg from "$lib/assets/images/tools/mysql.webp?enhanced";
     import PostgresImg from "$lib/assets/images/tools/postgres.webp?enhanced";
     import MondongoDBImg from "$lib/assets/images/tools/mondongodb.webp?enhanced";
+    import SqliteImg from "$lib/assets/images/tools/sqlite.png?enhanced";
     import RedisImg from "$lib/assets/images/tools/redis.webp?enhanced";
+    import NginxImg from "$lib/assets/images/tools/nginx.png?enhanced";
+    import ApacheImg from "$lib/assets/images/tools/apache.png?enhanced";
     import DockerImg from "$lib/assets/images/tools/docker.webp?enhanced";
+    import ArduinoImg from "$lib/assets/images/tools/arduino.png?enhanced";
+    import GitImg from "$lib/assets/images/tools/git.webp?enhanced";
 </script>
 
 <div class="tools-container">
-    <div class="tool-entry">
-        <enhanced:img src={RustImg} alt="" loading="lazy" />
-    </div>
+    <fieldset class="tools-section">
+        <legend>Langs</legend>
 
-    <div class="tool-entry">
-        <enhanced:img src={CImg} alt="" loading="lazy" />
-    </div>
+        <div title="Rust" class="tool-entry">
+            <enhanced:img src={RustImg} alt="" loading="lazy" />
+        </div>
+    
+        <div title="C" class="tool-entry">
+            <enhanced:img src={CImg} alt="" loading="lazy" />
+        </div>
+    
+        <div title="TypeScript" class="tool-entry">
+            <enhanced:img src={TypeScriptImg} alt="" loading="lazy" />
+        </div>
+    
+        <div title="JavaScript" class="tool-entry">
+            <enhanced:img src={JavaScriptImg} alt="" loading="lazy" />
+        </div>
 
-    <div class="tool-entry">
-        <enhanced:img src={TypeScriptImg} alt="" loading="lazy" />
-    </div>
+        <div title="Kotlin" class="tool-entry">
+            <enhanced:img src={KotlinImg} alt="" loading="lazy" />
+        </div>
+    
+        <div title="Python" class="tool-entry">
+            <enhanced:img src={PythonImg} alt="" loading="lazy" />
+        </div>
+    
+        <div title="Java" class="tool-entry">
+            <enhanced:img src={JavaImg} alt="" loading="lazy" />
+        </div>
+    
+        <div title="Go" class="tool-entry">
+            <enhanced:img src={GoImg} alt="" loading="lazy" />
+        </div>
 
-    <div class="tool-entry">
-        <enhanced:img src={JavaScriptImg} alt="" loading="lazy" />
-    </div>
+        <div title="Bash" class="tool-entry">
+            <enhanced:img src={BashImg} alt="" loading="lazy" />
+        </div>
+    </fieldset>
 
-    <div class="tool-entry">
-        <enhanced:img src={PythonImg} alt="" loading="lazy" />
-    </div>
+    <fieldset class="tools-section">
+        <legend>Libs / Frameworks</legend>
 
-    <div class="tool-entry">
-        <enhanced:img src={JavaImg} alt="" loading="lazy" />
-    </div>
+        <div title="Svelte" class="tool-entry">
+            <enhanced:img src={SvelteImg} alt="" loading="lazy" />
+        </div>
 
-    <div class="tool-entry">
-        <enhanced:img src={GoImg} alt="" loading="lazy" />
-    </div>
+        <div title="Vue" class="tool-entry">
+            <enhanced:img src={VueImg} alt="" loading="lazy" />
+        </div>
 
-    <div class="tool-entry">
-        <enhanced:img src={MySqlImg} alt="" loading="lazy" />
-    </div>
+        <div title="Angular" class="tool-entry">
+            <enhanced:img src={AngularImg} alt="" loading="lazy" />
+        </div>
 
-    <div class="tool-entry">
-        <enhanced:img src={PostgresImg} alt="" loading="lazy" />
-    </div>
+        <div title="NextJS" class="tool-entry">
+            <enhanced:img src={NextJsImg} alt="" loading="lazy" />
+        </div>
 
-    <div class="tool-entry">
-        <enhanced:img src={MondongoDBImg} alt="" loading="lazy" />
-    </div>
+        <div title="React" class="tool-entry">
+            <enhanced:img src={ReactImg} alt="" loading="lazy" />
+        </div>
 
-    <div class="tool-entry">
-        <enhanced:img src={RedisImg} alt="" loading="lazy" />
-    </div>
+        <div title="Bootstrap" class="tool-entry">
+            <enhanced:img src={BootstrapImg} alt="" loading="lazy" />
+        </div>
 
-    <div class="tool-entry">
-        <enhanced:img src={DockerImg} alt="" loading="lazy" />
-    </div>
+        <div title="Tailwind" class="tool-entry">
+            <enhanced:img src={TailwindImg} alt="" loading="lazy" />
+        </div>
 
-    <div class="tool-entry">
-        <enhanced:img src={GitImg} alt="" loading="lazy" />
-    </div>
+        <div title="Rocket.rs" class="tool-entry">
+            <enhanced:img src={RocketRsImg} alt="" loading="lazy" />
+        </div>
+    </fieldset>
 
-    <div class="tool-entry">
-        <enhanced:img src={BashImg} alt="" loading="lazy" />
-    </div>
+    <fieldset class="tools-section">
+        <legend>DBs</legend>
+
+        <div title="MySQL" class="tool-entry">
+            <enhanced:img src={MySqlImg} alt="" loading="lazy" />
+        </div>
+    
+        <div title="Postgres" class="tool-entry">
+            <enhanced:img src={PostgresImg} alt="" loading="lazy" />
+        </div>
+    
+        <div title="MondongoDB" class="tool-entry">
+            <enhanced:img src={MondongoDBImg} alt="" loading="lazy" />
+        </div>
+    
+        <div title="Redis" class="tool-entry">
+            <enhanced:img src={RedisImg} alt="" loading="lazy" />
+        </div>
+
+        <div title="SQLite" class="tool-entry">
+            <enhanced:img src={SqliteImg} alt="" loading="lazy" />
+        </div>
+    </fieldset>
+
+    <fieldset class="tools-section">
+        <legend>Tools</legend>
+
+        <div title="Nginx" class="tool-entry">
+            <enhanced:img src={NginxImg} alt="" loading="lazy" />
+        </div>
+
+        <div title="Apache" class="tool-entry">
+            <enhanced:img src={ApacheImg} alt="" loading="lazy" />
+        </div>
+
+        <div title="Docker" class="tool-entry">
+            <enhanced:img src={DockerImg} alt="" loading="lazy" />
+        </div>
+
+        <div title="Arduino" class="tool-entry">
+            <enhanced:img src={ArduinoImg} alt="" loading="lazy" />
+        </div>
+    
+        <div title="Git" class="tool-entry">
+            <enhanced:img src={GitImg} alt="" loading="lazy" />
+        </div>
+    </fieldset>
 </div>
 
 <style>
     .tools-container {
         display: flex;
+        flex-direction: column;
+        gap: var(--padding-m);
+    }
+
+    .tools-section {
+        display: flex;
+        justify-content: center;
         flex-wrap: wrap;
         gap: var(--padding-m);
+        border: var(--color-border) solid var(--border-width);
+        padding: var(--padding-m);
     }
 
     .tool-entry {
         height: 3rem;
-        width: 3rem;
     }
     .tool-entry img {
         height: 100%;
-        width: 100%;
+        width: auto;
     }
 </style>
